e4031be265696fdb2cef9e0fa64bef2a28ada232
André Ferreira <contato@andreferreira.me>
2020-04-26 16:28:12 -0300

    Add Ex1

---
+++ ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	printf("My name is %s\n", "André");
+
+	printf("This is an integer %i\n", 2);
+	
+	printf("This is a float number %f\n", 2.0);
+	
+	return 0;
+}
