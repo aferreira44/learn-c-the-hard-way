1bf9822b73a71c71b676c4109f7b5a435e1e138a
André Ferreira <contato@andreferreira.me>
2020-04-28 01:34:04 -0300

    Update ex9.c

--- ex9.c
+++ ex9.c
@@ -2,10 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 25;
-	while (i >= 0) {
-		printf("%d", i);
-		i--;
+	int i = 0;
+
+	// go throy=ugh each string in argv
+	// why am I skipping argv[0]?
+	for (i = 1; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+
+	// let's make our own array of strings
+	char *states[] = {
+		"California", "Oregon",
+		"Washginton", "Texas"
+	};
+
+	int num_states = 4;
+
+	for (i = 0; i < num_states; i++) {
+		printf("state %d: %s\n", i, states[i]);
 	}
 
 	return 0;
