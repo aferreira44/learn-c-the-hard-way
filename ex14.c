bc49f14862b79f748f9a7ca446ff580785440397
André Ferreira <contato@andreferreira.me>
2020-04-30 14:18:25 -0300

    Writing and Using Functions

---
+++ ex14.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+// forward declarations
+void print_letters(int argLen, char arg[]);
+
+void print_arguments(int argc, char *argv[])
+{
+    int i = 0;
+
+    for (i = 0; i < argc; i++)
+    {
+        print_letters(strlen(argv[i]), argv[i]);
+    }
+}
+
+void print_letters(int argLen, char arg[])
+{
+    int i = 0;
+
+    for (i = 0; i < argLen; i++)
+    {
+        char ch = arg[i];
+
+        if (isalpha(ch) || isblank(ch))
+        {
+            printf("'%c' == %d\n", ch, ch);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    print_arguments(argc, argv);
+
+    return 0;
+}
